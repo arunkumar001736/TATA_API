9ed63e18e8abbe305fec365981eecb7fae42e2e6
arunkumar001736 <arunkumar2002017@srec.ac.in>
2024-11-19 21:50:36 +0530

    TATA first commit

---
+++ models/model_bk.js
@@ -0,0 +1,28 @@
+
+const { DataTypes } = require('sequelize');
+//const sequelize = require('./database');
+
+const sequelize = require('../config/database');
+
+
+
+const YourTable = sequelize.define('Data', {
+    ID: {
+        type: DataTypes.INTEGER,
+        allowNull: true    ,
+        primaryKey: true 
+    },
+    Name: {
+        type: DataTypes.STRING,
+        allowNull: true 
+    },
+    Email_ID: {
+        type: DataTypes.STRING,
+        allowNull: true 
+    }
+}, {
+    tableName: 'Data',  
+    timestamps: false        
+});
+
+module.exports=YourTable
